23a76fa40fb31aeef978d539dabc5c3cf9afd133
The Vinh <89290241+ntvinh2005@users.noreply.github.com>
2021-12-28 17:37:52 +0700

    Fix cors

--- server/index.js
+++ server/index.js
@@ -2,6 +2,11 @@ require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
 
 const authRouter = require('./routes/auth')
 const noteRouter = require('./routes/note')
@@ -26,7 +31,7 @@ connectDB()
 
 const app = express()
 app.use(express.json());
-app.use(cors())
+app.use(cors(corsOptions))
 
 app.get('/', (req, res)=>res.send('Hello world'))
 app.use('/api/auth', authRouter)
