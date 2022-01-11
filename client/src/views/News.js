4c2abab09d221a0f01fe862dab133c01e81b3a0a
The Vinh <89290241+ntvinh2005@users.noreply.github.com>
2022-01-11 22:09:32 +0700

    Repair data from api

--- client/src/views/News.js
+++ client/src/views/News.js
@@ -57,7 +57,7 @@ const Assistant = () => {
                 {newsData.articles.map(
                     (article, index) => (
                         <div key={index + '-' + Date.now()} className="Weather-card mb-3">
-                            <a className="link" target="_blank" href={article.url}>
+                            <a className="link" target="_blank" href={article.link}>
                             <div className="Modal-start-end p-3">
                                 <h5>{index+1}</h5>
                                 <p>
@@ -69,39 +69,12 @@ const Assistant = () => {
                             </div>
                             <div className="Modal-body p-5">
                                 <p>{article.summary}</p>
-                                <p className='Card-body'>{article.publishedAt == undefined ? null : String(article.publishedAt.split("T")[0])}</p>
+                                <p className='Card-body'>{article.published_date == undefined ? null : String(article.published_date.split("T")[0])}</p>
                             </div></a>
                         </div>
                     )
                 )}<hr/>
                 
-                {/*<h1 className="mb-4">
-                <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" className="bi bi-pin-angle-fill me-3" viewBox="0 0 16 16">
-                    <path d="M9.828.722a.5.5 0 0 1 .354.146l4.95 4.95a.5.5 0 0 1 0 .707c-.48.48-1.072.588-1.503.588-.177 0-.335-.018-.46-.039l-3.134 3.134a5.927 5.927 0 0 1 .16 1.013c.046.702-.032 1.687-.72 2.375a.5.5 0 0 1-.707 0l-2.829-2.828-3.182 3.182c-.195.195-1.219.902-1.414.707-.195-.195.512-1.22.707-1.414l3.182-3.182-2.828-2.829a.5.5 0 0 1 0-.707c.688-.688 1.673-.767 2.375-.72a5.922 5.922 0 0 1 1.013.16l3.134-3.133a2.772 2.772 0 0 1-.04-.461c0-.43.108-1.022.589-1.503a.5.5 0 0 1 .353-.146z"/>
-                </svg>
-                    More News
-                </h1>
-
-                {commonNewsData.articles.map(
-                    (article, index) => (
-                        <div key={index + '-' + Date.now()} className="Weather-card mb-3">
-                            <a className="link" target="_blank" href={article.url}>
-                            <div className="Modal-start-end p-3">
-                                <h5>{index+1}</h5>
-                                <p>
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-pin-fill me-2" viewBox="0 0 16 16">
-                                    <path d="M4.146.146A.5.5 0 0 1 4.5 0h7a.5.5 0 0 1 .5.5c0 .68-.342 1.174-.646 1.479-.126.125-.25.224-.354.298v4.431l.078.048c.203.127.476.314.751.555C12.36 7.775 13 8.527 13 9.5a.5.5 0 0 1-.5.5h-4v4.5c0 .276-.224 1.5-.5 1.5s-.5-1.224-.5-1.5V10h-4a.5.5 0 0 1-.5-.5c0-.973.64-1.725 1.17-2.189A5.921 5.921 0 0 1 5 6.708V2.277a2.77 2.77 0 0 1-.354-.298C4.342 1.674 4 1.179 4 .5a.5.5 0 0 1 .146-.354z"/>
-                                </svg>
-                                    {article.title}
-                                </p>
-                            </div>
-                            <div className="Modal-body p-5">
-                                <p>{article.description}</p>
-                                <p className='Card-body'>{article.publishedAt == undefined ? null : String(article.publishedAt.split("T")[0])}</p>
-                            </div></a>
-                        </div>
-                    )
-                    )}*/}
                 </div>
                 <br/>
                 
